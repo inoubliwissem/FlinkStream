816ab5ed53bbd1b0d281f2dd3086b84f1240745d
inoubliwissem <inoubliwissem@gmail.com>
2016-03-10 16:16:21 +0100

    Update streamingkafka.java

--- src/main/java/com/flink/streamingkafka.java
+++ src/main/java/com/flink/streamingkafka.java
@@ -1,11 +1,13 @@
-
 package com.flink;
 import java.util.Properties;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer082;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.util.Collector;
 
 public class streamingkafka {
 
@@ -16,11 +18,27 @@ public class streamingkafka {
         props.put("zookeeper.connect", "localhost:2181");
         props.put("group.id", "group_test");
 
-        DataStream<String> messageStream = env.addSource(new FlinkKafkaConsumer082<>("test", new SimpleStringSchema(), props));
-        messageStream.print();
+        DataStream<String> messageStream = env
+                .addSource(new FlinkKafkaConsumer082<>("test", new SimpleStringSchema(), props));
 
+        DataStream<Tuple2<String, Integer>> dataStream2 = messageStream.flatMap(new streamingkafka.Splitter())
+                .groupBy(0)
+                .sum(1);
+        dataStream2.print();
 
         env.execute();
     }
 
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word : sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+
+    }
+
 }
+
